2441acde5112e471419c67af5464ed7ddd99983d
Zekumoru Dragonhart <dev@zekumoru.com>
2022-09-05 11:38:53 +0200

    Add LibraryRenderer and its add function

--- script.js
+++ script.js
@@ -45,8 +45,65 @@ const AddBookDialog = (() => {
   return Object.assign(object, {
     show,
     hide,
+    clear,
     onsubmit,
   });
+})();
+
+const LibraryRenderer = (() => {
+  const display = document.querySelector('.main');
+  const info = document.querySelector('.main .info');
+
+  const list = document.createElement('ul');
+  list.classList.add('cards');
+
+  const onchange = () => {
+    if (list.childElementCount) {
+      info.style.display = 'none';
+      display.classList.remove('flex-center-vertical');
+      display.appendChild(list);
+      return;
+    }
+    
+    display.removeChild(list);
+    info.style.display = 'block';
+    display.classList.add('flex-center-vertical');
+  };
+
+  function add(book) {
+    list.appendChild(createCard(book));
+    onchange();
+  }
+
+  function remove() {
+    list.removeChild(list.firstChild);
+    onchange();
+  }
+
+  function createCard(book) {
+    const card = document.createElement('li');
+    card.classList.add('card');
+    card.innerHTML = `
+      <button class="read">Unread</button>
+      <div class="content">
+        <h2 class="title">${book.name}</h2>
+        <p class="author">${book.author} (${book.pages} pages)</p>
+      </div>
+      <div class="buttons">
+        <button>
+          <div class="icon edit"></div>
+        </button>
+        <button>
+          <div class="icon delete"></div>
+        </button>
+      </div>`;
+    return card;
+  }
+
+  return {
+    add,
+    remove,
+  };
 })();
 
 document.querySelector('header button.add').addEventListener('click', () => {
@@ -54,5 +111,7 @@ document.querySelector('header button.add').addEventListener('click', () => {
 });
 
 AddBookDialog.onsubmit = (book) => {
-  console.log(book.name, book.author, book.pages, book.read);
+  LibraryRenderer.add(book);
+  AddBookDialog.clear();
+  AddBookDialog.hide();
 };
